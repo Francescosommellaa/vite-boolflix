f4147063b2809c6c9f9d7e35b7ef93d90d037dcf
Francescosommellaa <franc.sommella@gmail.com>
2024-07-12 10:58:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+// import store
+import { store } from './store';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<template>
+    <AppHeader />
+</template>
+
+<style lang="scss">
+  @use './styles/partials/mixins' as *;
+  @use './styles/partials/variables' as *;
+  @use './styles/general.scss' as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+    name: "AppHeader",
+};
+
+</script>
+
+<template>
+
+<div>
+
+</div>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as * ;
+@use '../styles/partials/variables' as * ;
+</style>
